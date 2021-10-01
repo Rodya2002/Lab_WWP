f6376452df17e761342189deda935898b3874228
Rodion <sabirov200208@mail.ru>
2021-10-02 02:27:16 +0300

    Добавил лабу 4

---
+++ Lab_4/Лаба4/Лаба4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+	//Пункт 1 
+	double a, b;
+
+	cout << "Wwedite dliny storon pryamougolnika a i b" << endl;
+
+	cin >> a; 
+	cin >> b;
+
+	cout << "Ploshad = " << a * b << endl;
+	cout << "Perimetr = " << 2 * (a + b) << endl << endl;
+
+	//Пункт 2 
+
+	double Pi = 3.14;
+	double D;
+
+	cout << "Wwedite diametr okruzhosti" << endl;
+
+	cin >> D;
+
+	cout << "Dlina okruzhosti = " << Pi * D << endl <<endl;
+
+	//Пункт 3
+
+	cout << "Nayti srednee arifmeticheskoe" << endl;
+
+	cin >> a;
+	cin >> b;
+
+	cout << "Srednya arifmeticheskay = " << (a + b) / 2 << endl;
+	
+	//Пункт 4
+
+
+
+
+
+	return 0;
+}

3c341f8d59bc763e1715278954dd1b7ad30454af
Rodion <sabirov200208@mail.ru>
2021-11-09 11:42:50 +0300

    Пункт 1
    Сделал пункт 1

---
+++ Lab_5/Lab5p1/Пункт 1/Пункт 1/Source.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SCURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int x1, x2, y1, y2;
+	float n;
+	scanf("%d %d", &x1, &y1);
+	scanf("%d %d", &x2, &y2);
+	n = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	printf("%f", n);
+}

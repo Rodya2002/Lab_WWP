f9792152e2243734de59e493a8de25b593d053a6
Rodion <sabirov200208@mail.ru>
2021-11-09 11:52:53 +0300

    Пункт 2
    Сделал пункт 2

---
+++ Lab_5/Lab5p2/Пункт2/Пункт2/Source.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SCURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a, b, c;
+	scanf("%f %f %f", &a, &b, &c);
+	printf("%f %f %f", ûc - a, c - b, c - a + c - b);
+}

bf2d444d5c73133a0a61b6fec5c73d81db295bc4
Rodion <sabirov200208@mail.ru>
2021-11-19 21:16:56 +0300

    Сделал пункт 6 и 7

---
+++ Lab_7/Пункт6/Лаб7/Лаб7/Лаб7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+int main()
+{
+
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double A, B, C;
+    double temp1, temp2;
+
+
+
+
+    //6
+    double a;
+    double powa;
+
+    std::cout << "Введите значение А\n";
+    std::cin >> a;
+
+    powa = a * a;
+
+    std::cout << "Значение А в 8 степени = " << powa * powa * powa * powa << "\n\n";
+}
---
+++ Lab_7/Пункт7/Лаба7/Лаба7/Лаба7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+int main()
+{
+
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    double A, B, C;
+    double temp1, temp2;
+    double a, powa;
+    //7
+    std::cout << "Введите значение А\n";
+    std::cin >> a;
+
+    powa = a * a;
+    int powpowa = powa * powa;
+
+    std::cout << "Значение А в 15 степени = " << powpowa * powpowa * powpowa * a * a * a << "\n\n";
+}
